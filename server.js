59b5456e1f3be83f649e59a8365a1e1b9e4cddf4
Hong Anh Dinh <dinhhonganh.hcm@gmail.com>
2024-07-13 10:45:16 +0700

    Hello world with express js

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
-const http = require("http");
-const hostname = "127.0.0.1"; //cái này === http://localhost
-const port = 3000;
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
-    //Set the response HTTP header with HTTP status and Content type
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World\n");
+const express = require('express');
+const app = express();
+const port = 8081n;
+
+app.get('/', (req, res) => {
+    res.send('Hello, world');
 });
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
-//tự động chạy trên localhost
-// server.listen(port, () => {
-// console.log(`Server running at http://${hostname}:${port}/`);
